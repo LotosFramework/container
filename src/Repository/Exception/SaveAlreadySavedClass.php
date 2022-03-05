d18e9240371642d5fbfb732e10c5d79404a34256
Sergey Astapenko <mclotos@xakep.ru>
2022-03-05 12:56:24 +0200

    remove Repository

--- src/Repository/Exception/SaveAlreadySavedClass.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-namespace Lotos\Container\Repository\Exception;
-
-class SaveAlreadySavedClass extends RepositoryException
-{}
--- src/Repository/Exception/WrongArgumentTypeException.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-namespace Lotos\Container\Repository\Exception;
-
-class WrongArgumentTypeException extends RepositoryException
-{}
